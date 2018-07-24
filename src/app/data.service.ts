a9024bcca0f08f221b93ac957dd2451620575da2
Luna Ayala <LLUNA@t-systems.es>
2018-07-24 11:51:21 +0200

    first commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable()
+
+export class DataService {
+
+	private goals = new BehaviorSubject<any>(['primero','otro']);
+	goal = this.goals.asObservable();
+
+	constructor() { }
+
+	changeGoal(goal) {
+		this.goals.next(goal);
+	}
+
+
+}
